676347625d87130b31bf2aef3c3a81aab05f8ab7
avollkopf <43980694+avollkopf@users.noreply.github.com>
2022-06-13 21:18:59 +0200

    Test with variable actor power
    e.g. one element with 1200 watt and one with 1800 watt

--- cbpi4-GroupedPowerActor/__init__.py
+++ cbpi4-GroupedPowerActor/__init__.py
@@ -13,13 +13,21 @@ logger = logging.getLogger(__name__)
 
 
 @parameters([Property.Actor(label="Actor01", description="Select an actor to be controlled by this group."),
+            Property.Number(label="Power01", description="Enter Power for Actor01 if Actors in group have different power."),
             Property.Actor(label="Actor02", description="Select an actor to be controlled by this group."),
+            Property.Number(label="Power02", description="Enter Power for Actor02 if Actors in group have different power."),
             Property.Actor(label="Actor03", description="Select an actor to be controlled by this group."),
+            Property.Number(label="Power03", description="Enter Power for Actor03 if Actors in group have different power."),
             Property.Actor(label="Actor04", description="Select an actor to be controlled by this group."),
+            Property.Number(label="Power04", description="Enter Power for Actor04 if Actors in group have different power."),
             Property.Actor(label="Actor05", description="Select an actor to be controlled by this group."),
+            Property.Number(label="Power05", description="Enter Power for Actor05 if Actors in group have different power."),
             Property.Actor(label="Actor06", description="Select an actor to be controlled by this group."),
+            Property.Number(label="Power06", description="Enter Power for Actor06 if Actors in group have different power."),
             Property.Actor(label="Actor07", description="Select an actor to be controlled by this group."),
-            Property.Actor(label="Actor08", description="Select an actor to be controlled by this group.")])
+            Property.Number(label="Power07", description="Enter Power for Actor07 if Actors in group have different power."),
+            Property.Actor(label="Actor08", description="Select an actor to be controlled by this group."),
+            Property.Number(label="Power08", description="Enter Power for Actor08 if Actors in group have different power.")])
 
 class GroupedPowerActor(CBPiActor):
 
@@ -36,40 +44,68 @@ class GroupedPowerActor(CBPiActor):
     def on_start(self):
         self.state = False
         self.actors = []
+        self.maxactorpower = []
         self.powersum = []
         self.actorpower = []
+        self.actorportion= []
         self.power = 100
+        self.totalpower = 0
         logging.info("GROUPED POWER ACTOR")
         if self.props.get("Actor01", None) is not None:
             self.actors.append(self.props.get("Actor01"))
+            self.maxactorpower.append(int(self.props.get("Power01",1)))
+            self.totalpower = self.totalpower + self.maxactorpower[0]
         if self.props.get("Actor02", None) is not None:
             self.actors.append(self.props.get("Actor02"))
+            self.maxactorpower.append(int(self.props.get("Power02",1)))
+            self.totalpower = self.totalpower + self.maxactorpower[1]
         if self.props.get("Actor03", None) is not None:
             self.actors.append(self.props.get("Actor03"))
+            self.maxactorpower.append(int(self.props.get("Power03",1)))
+            self.totalpower = self.totalpower + self.maxactorpower[2]
         if self.props.get("Actor04", None) is not None:
             self.actors.append(self.props.get("Actor04"))
+            self.maxactorpower.append(int(self.props.get("Power04",1)))
+            self.totalpower = self.totalpower + self.maxactorpower[3]
         if self.props.get("Actor05", None) is not None:
             self.actors.append(self.props.get("Actor05"))
+            self.maxactorpower.append(int(self.props.get("Power05",1)))
+            self.totalpower = self.totalpower + self.maxactorpower[4]
         if self.props.get("Actor06", None) is not None:
             self.actors.append(self.props.get("Actor06"))
+            self.maxactorpower.append(int(self.props.get("Power06",1)))
+            self.totalpower = self.totalpower + self.maxactorpower[5]
         if self.props.get("Actor07", None) is not None:
             self.actors.append(self.props.get("Actor07"))
+            self.maxactorpower.append(int(self.props.get("Power07",1)))
+            self.totalpower = self.totalpower + self.maxactorpower[6]
         if self.props.get("Actor08", None) is not None:
             self.actors.append(self.props.get("Actor08"))
+            self.maxactorpower.append(int(self.props.get("Power08",1)))
+            self.totalpower = self.totalpower + self.maxactorpower[7]
         self.actornum=len(self.actors)
-        self.actorportion=100*(float(1/self.actornum))
+        #logging.info(self.actors)
+        #logging.info(self.actornum)
+        #logging.info(self.totalpower)
+        #logging.info(self.maxactorpower)
+        #self.actorportion=100*(float(1/self.actornum))
         i=0
         while i < self.actornum:
-            self.powersum.append(round(self.actorportion*(i+1)))
+            self.actorportion.append(100*float(self.maxactorpower[i]/self.totalpower))
+            if i == 0:
+                power=self.actorportion[i]
+            else:
+                power=self.powersum[i-1]+self.actorportion[i]
+            self.powersum.append(power)
             self.actorpower.append(0)
             i+=1
-
+        logging.info(self.actorportion)
+        logging.info(self.powersum)
         pass
 
     async def on(self, power=None):
         if power is not None:
             self.power = power
-
         self.calculate_individual_actor_power(self.power)
         i=0
         for actor in self.actors:
@@ -107,9 +143,14 @@ class GroupedPowerActor(CBPiActor):
         while i < self.actornum:
             self.actorpower.append(0)
             i+=1
+        #logging.info(self.actorpower)
         idx = np.searchsorted(self.powersum, power, side="left")
-        max_actor_power=int(self.powersum[idx])
+        #logging.info(idx)
+        #logging.info(self.powersum)
+        max_actor_power=int(self.powersum[idx-1])
+        #logging.info(max_actor_power)
         active_actor_power = round(100*power/max_actor_power)
+        #logging.info(active_actor_power)
         if idx == 0:
             self.actorpower[0]=active_actor_power
         else:
@@ -119,14 +160,14 @@ class GroupedPowerActor(CBPiActor):
                 self.actorpower[i]=100
                 i+=1
             remaining_actor_power=power-fixed_actor_power
-            variable_actor_power = round(100*remaining_actor_power/round(self.actorportion))
+            variable_actor_power = round(100*remaining_actor_power/round(self.actorportion[i]))
             if int(variable_actor_power) > 100:
                 variable_actor_power = 100
             if int(variable_actor_power) < 0:
                 variable_actor_power = 0           
             self.actorpower[idx]=variable_actor_power
 
-        logging.info(self.actorpower)
+        #logging.info(self.actorpower)
         pass
 
 
